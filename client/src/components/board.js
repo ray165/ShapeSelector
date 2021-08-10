26173aedec70f6eff5b4d470fd013092c15e0fb6
Raymond <70726900+ray165@users.noreply.github.com>
2021-08-09 23:48:32 -0700

    Starting from a guide

---
+++ client/src/components/board.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+// from the guide https://blog.ranaemad.com/whiteboard-react-hooks-ckclrvccg0005fls16f1h80mc
+
+function Board() {
+
+    return (
+
+    )
+}
+
+export default Board 
